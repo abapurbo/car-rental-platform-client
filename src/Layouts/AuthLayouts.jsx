aca7d53479f72705de39c76ccd305a601cc146ba
apurbo <absarker238@gmail.com>
2025-11-08 23:03:58 -0800

    my rentWheels plartform

---
+++ src/Layouts/AuthLayouts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function AuthLayouts() {
+  return <div>
+    {/* for login and signup pages */}
+  </div>;
+}
---
+++ src/Pages/Home/FeaturedCars.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FeaturedCars() {
+  return <div>FeaturedCars</div>;
+}
