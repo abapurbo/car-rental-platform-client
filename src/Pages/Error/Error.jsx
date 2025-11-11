945f01c9d45b64a23f7cdc3e9e1e79f33c365dcd
apurbo <absarker238@gmail.com>
2025-11-11 14:16:28 -0800

    added error page lottie-react

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import Lottie from "lottie-react";
+import error from '../../assets/Lottie/error.json'
 export default function Error() {
-  return <div>Error</div>;
+  return <div className="h-screen container mx-auto flex justify-center items-center">
+    <div className="">
+      <Lottie animationData={error} />
+    </div>
+  </div>
 }
